08a202909706d11109d19529d25c3a4c20ee2694
Decix <33365193+Derpyturtle99@users.noreply.github.com>
2018-06-03 16:19:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,33 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// CONFIG COMMANDS
+client.on('message', message => {
+    const args = message.content.slice(process.prefix).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const gameSet = args.join(" ");
+    
+    if (command === '$setgame') {
+        if (gameSet === 'reset' || message.content === '$setgame') {
+            message.reply('Game has been reset!')
+            client.user.setPresence({ game: { name: null, type: 0} });
+        } else {
+            client.user.setPresence({ game: { name: gameSet, type: 0} });
+            message.reply('Game set to: ``' + (gameSet) + '``');
+        }
+    }
+        
+
+
 // COMMANDS
 client.on('message', message => {
   if(message.author.bot) return;
   const args = message.content.slice(process.prefix).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  const gameSet = args.join(" ");
   
   // Say Command
   if (command === '$say') {
-       const sayMessage = args.join(" ");
+      const sayMessage = args.join(" ");
       message.delete().catch(O_o=>{}); 
       message.channel.send(sayMessage);
     }
