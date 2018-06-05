462a0a70ee5bab84ebc8d15f4f65c8d528513272
Decix <33365193+Derpyturtle99@users.noreply.github.com>
2018-06-04 21:22:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ function resetBot(channel) {
     // send channel a message that you're resetting bot [optional]
     channel.send('Resetting...')
     .then(msg => client.destroy())
-    .then(() => client.login(<your bot token here>));
+    .then(() => client.login(process.env.BOT_TOKEN));
 }
 
 
